cb19a6ba000f5951ad415e5adcc5172216a35eaf
Diana Barber aka CodingNannah <codingnannah@gmail.com>
2025-02-08 02:08:49 -0700

    adding html and quotes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # First 100 Days of Coding Project
-From Tech with Tim Tutorial https://www.youtube.com/watch?v=2ml4x0rO1PQ at 1:45:08
+From Tech with Tim Tutorial https://www.youtube.com/watch?v=2ml4x0rO1PQ at 45:49
 
 ## Purpose
 Practice JS with a random quote generator
---
+++ script.js
@@ -0,0 +1,45 @@
+const quotes = [
+    '"Life is hard. After all, it kills you." - Katharine Hepburn',
+    '"The secret of staying young is to live honestly, eat slowly, and lie about your age." - Lucille Ball',
+    '"Those people who think they know everything are a great annoyance to those of us who do." - Isaac Asimov',
+    '"The only place success comes before work is in the dictionary." - Vince Lombardi',
+    '"I want my children to have all the things I could not afford. Then, I want to move in with them." - Phyllis Diller',
+    '"Before you criticize someone, you should walk a mile in their shoes. That way, when you criticize them, you are a mile away from them, and you have their shoes." - Jack Handey',
+    '"When you are asked if you can do a job, tell them: Certainly, I can! Then, get busy and find out how to do it." - Theodore Roosevelt',
+    '"Hard work never killed anybody, but why take a chance?" - Bergen',
+    '"I generally avoid temptation, unless I cannot resist it." - Mae West',
+    '"A day without sunshine is like, you know, night." - Steve Martin',
+    '"When I was growing up, I always wanted to be someone. Now, I realize I should have been more specific." - Lily Tomlin',
+    '"Never put off till tomorrow what may be done day after tomorrow just as well." - Mark Twain',
+    '"People say nothing is impossible, but I do nothing everyday." - Winnie the Pooh',
+    '"True friends are those who really know you but love you anyway." - Edna Buchanan',
+    '"Whatever you do, always give 100% -- unless you are donating blood." Bill Murray',
+    '"Be like a postage stamp; stick to one thing until you get there." - Josh Billings',
+    '"The difference between try and triumph is just a little umph!" - Marvin Phillips',
+    '"You can only come to the morning through the shadows." - J.R.R. Tolkien',
+    '"My therapist told me the way to achieve true inner peace is to finish what I start. So far, I have finished two bags of M&Ms and a chocolate cake. I feel better already!" - Dave Barry',
+    '"Do not count the days. Make the days count!" - Muhammad Ali',
+    '"To succeed in life, you need three things: a wishbone, a backbone, and a funny bone." - Reba McEntire',
+    '"Lots of people want to ride with you in the limo, but what you want is someone who will take the bus with you when the limo breaks down." - Oprah Winfrey',
+    '"One day, you will wake up and there will not be anymore time to do the things you have always wanted. Do it now!" - Paulo Coelho',
+    '"If today were the last day of my life, would I want to do what I am about to do today?" - Steve Jobs',
+    '"Most of us do not need a psychiatric therapist as much as a friend to be silly with." - Robert Brault',
+    '"Lose an hour in the morning, and you will spend all day looking for it." - Richard Whately',
+    '"A loyal friend laughs at your jokes when they are not so good and sympathizes with your problems when they are not so bad." - Arnold H. Glasgow',
+    '"A true friend knows you are a good egg, even though he knows that you are slightly cracked." - Bernard Meltzer',
+    '"You have got to get up every morning with determination if you are going to bed with satisfaction." - George Lorimer',
+    '"You are not fully dressed, until you wear a smile." - Evan Esar',
+    '"If you think you are too small to be effective, you have never been in bed with a mosquito." - Betty Reese',
+    '"All you need is love, but a little chocolate now and then does not hurt." - Charles M. Schulz',
+    '"If you are going through hell, keep going." - Winston Churchill',
+    '"When you reach the end of your rope, tie a know in it and hang on." - Franklin D. Roosevelt',
+    '"I dream of a better tomorrow, where chickens can cross the road and not be questioned about their motives." - Ralph Waldo Emerson',
+]
+
+const usedIndexes = new Set()
+const quoteElement = document.getElementById("quote")
+
+function generateQuote() {
+    // quoteElement.innerHTML = "Hello, World!"
+
+}
